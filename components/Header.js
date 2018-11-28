abbb054186416f0f5089d45441ddc4979109032b
Jake Bian <jake@thousandeyes.com>
2018-11-28 17:24:09 +0000

    remove empty about page for now

--- components/Header.js
+++ components/Header.js
@@ -7,7 +7,7 @@ const linkStyle = {
 const Header = (props) => (
     <div>
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css"/>
-
+        <title></title>
         <div className='header-row'>
         <div className='otim-logo'>
         <a href="/" style={linkStyle}>
@@ -17,10 +17,10 @@ const Header = (props) => (
         </div>
         <div className="otim-nav">
         <a className='github-link' href="https://github.com/otim-project/">github</a>
-        <Link href="/about">
+{/*        <Link href="/about">
           <a style={linkStyle}>about</a>
         </Link>
-        </div>
+*/}        </div>
         </div>
 
         <style jsx>{`
