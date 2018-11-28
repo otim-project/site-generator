cdc11c915e292d9ffe1a58f5b0bb7585988ccde3
Jake Bian <jake@thousandeyes.com>
2018-11-28 16:43:53 +0000

    feat: show links on nodes page

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,26 @@ const Header = () => (
     <div>
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css"/>
 
+        <div className='header-row'>
         <div>OTIM</div>
+        <div className="otim-nav">
         <a href="/" style={linkStyle}>Home</a>
         <Link href="/about">
           <a style={linkStyle}>About</a>
         </Link>
+        </div>
+        </div>
+
+        <style jsx>{`
+            .otim-nav{
+                float: right;
+            }
+
+            .header-row {
+                height: 100px;
+            }
+        `}</style>
+
     </div>
 )
 
--- pages/nodePage.js
+++ pages/nodePage.js
@@ -1,9 +1,108 @@
 import Layout from '../components/MyLayout.js'
 
-export default (props) => (
+export default ({url: {query: {meta, staticDir, config, content}}}) => (
     <Layout>
-        <pre>
-       {JSON.stringify(props.url.query, null, 4)}
-       </pre>
+        <div>
+            <div className="node-header">
+                <div className="node-repo-container">
+                <a className="node-repo" href={`https://github.com/${config.repo}`}>{config.repo}</a>
+                </div>
+                <div className="node-title">{meta.title}</div>
+                <div className="node-author">{meta.author}</div>
+                <div className="node-date">Published {meta.date}</div>
+            </div>
+            <div className="content">
+                {renderContent(content, staticDir)}
+            </div>
+            <style jsx>{`
+                .content {
+                    max-width: 700px;
+                    margin: auto;
+                }
+                .node-header {
+                    max-width: 750px;
+                    margin: auto;
+                    padding-top: 50px;
+                    padding-bottom: 50px;
+                }
+                .node-title {
+                    font-size: 2.5em;
+                }
+
+                .node-author {
+                    font-size: 1.5em;
+                    margin: 20px 0;
+                }
+
+                .node-date {
+                    color: rgba(0, 0, 0, 0.3);
+                    margin: 10px 0;
+                    display: inline-block;
+                }
+                .node-repo-container {
+                    text-align: right;
+                    display: block;
+                    margin-bottom: 150px;
+                }
+                .node-repo {
+                    text-align: right;
+                    font-size: .8em;
+                    padding: 30px;
+                    border-radius: 5px;
+                    box-shadow: 10px 26px 88px -15px rgba(0,0,0,0.1);
+                    color: #fff;
+                    background-color: #f4b042;
+                    margin: auto;
+                    transition: all 0.5s ease;
+                    letter-spacing: 2px;
+                }
+                .node-repo:hover {
+                  opacity: .5;
+                }
+
+            `}</style>
+
+        </div>
     </Layout>
 )
+
+function renderContent(content, staticDir) {
+    return content.map(
+        ({title, path, sub}) => (
+            <div className='chapter-container'>
+                <div className='chapter-header'>
+                <div className='chapter-name'>{title}</div>
+                {
+                    path && staticDir[path] ? (
+                        <a className='chapter-link' href={`/static/built/${staticDir[path]}`}>pdf</a>
+                    ) : ''
+                }
+                </div>
+                {
+                    sub ? (
+                        <div className='sub-chapter'>
+                            {renderContent(sub, staticDir)}
+                        </div>
+                    ): ''
+                }
+
+                <style jsx>{`
+                    .chapter-header {
+                        padding: 10px;
+                        border-bottom: 1px solid rgba(0, 0, 0, 0.05);
+                    }
+                    .chapter-name {
+                        display: inline-block;
+                    }
+                    .chapter-link {
+                        margin-left: 10px;
+                    }
+                    .sub-chapter {
+                        margin-left: 30px;
+                    }
+                `}</style>
+
+            </div>
+        )
+    )
+}
